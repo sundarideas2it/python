f12d5f97e9a36027d8b87e23ee57ecac8be19c0c
sundarideas2it <sundar@ideas2it.com>
2017-01-25 21:01:36 +0530

    fibonacci function updated

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,12 @@
-'''
-	Filename: fibonacci.py
+"""	Filename: fibonacci.py
 	Author: Sundar P
 	Created On: 10 Jan 2017	
 	Description: Fibonacci sequence
-'''
-n=10
+"""
+print(__doc__)
+
+n, a, b=10, 0, 1
 print('Print Fibonacci sequence upto', n) #print is used display the given values
-a=0
-b=1
-c=(a+b)
-print(a)
-while(c < 10):
-	print(c)
-	c=(a+b)	
-	a=b
-	b=c
-print("End")
+while(b < n):
+	print(b)
+	a, b=b, a+b
