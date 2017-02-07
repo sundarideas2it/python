b5431c9f617a476b8b3ea78b553538459c4c055f
sundarideas2it <sundar@ideas2it.com>
2017-02-07 21:36:02 +0530

    Modules & Executing modules

---
+++ importfibomodule.py
@@ -0,0 +1,10 @@
+"""	Filename: importfibomodule.py
+	Author: Sundar P
+	Created On: 07 Feb 2017	
+	Description: Import fibo module
+"""
+import fibomodule
+from fibomodule import fib
+
+print (fibomodule.fib2(10))
+print (fib(50))
